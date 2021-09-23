535dccc20cc5f272c6f2502b22a3556d4ffe687e
stabilaprotocol <daniel.varzari@gmail.com>
2021-09-23 15:46:20 +0300

    mod

--- cpp/CMakeFiles/rust.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../rust/librustzcash/target"
-  "CMakeFiles/rust"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rust.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
